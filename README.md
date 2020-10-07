3a346f01a7978124f512a3c38d1f903f3482c93e
Hosea Eyob <hosea.eyob@macbook-pro.lan>
2020-10-06 23:22:37 -0500

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+"Students" Lab
+Submitted 10.06.2020
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ src/Student.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Student = (props) => {
+    return(
+        <div>
+            {props.student.name}
+        </div>
+    );
+}
+
+
+
+export default Student;
